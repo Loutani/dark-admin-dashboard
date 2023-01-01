e33c1feba9c39d70248ce5ec8ecd012adca36db8
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2023-01-01 11:32:29 +0100

    create Breadcrumb component

---
+++ src/components/Breadcrumb/Breadcrumb.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Breadcrumb({children}) {
+    return (
+        <div className='breadcrumb'>
+            {children}
+        </div>
+    )
+}
+
+export default Breadcrumb
