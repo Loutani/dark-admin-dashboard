8da0ba0eec3bea33c32343a008f6945bf17a05e7
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2023-01-01 12:08:44 +0100

    create DateInput component

---
+++ src/components/DateInput/DateInput.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function DateInput({label, htmlFor}) {
+    return (
+        <div className='date-input'>
+            <label htmlFor={htmlFor}>{label}</label>
+            <input id={htmlFor} type='date' />
+        </div>
+    )
+}
+
+export default DateInput
