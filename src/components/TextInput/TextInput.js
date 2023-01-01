cad2f825690827e45c0451bab5468cecb699ec6b
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2023-01-01 12:01:54 +0100

    create TextInput component

---
+++ src/components/TextInput/TextInput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TextInput({label, htmlFor, type, classname}) {
+
+    return (
+        <div className={'text-input ' + classname}>
+            <label htmlFor={htmlFor}>{label}</label>
+            <input id={htmlFor} type={type} />
+        </div>
+    )
+}
+
+export default TextInput
